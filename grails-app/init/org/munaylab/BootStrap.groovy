ac0b33d1bf4392a0531741b9e8631f9237d808ce
Augusto Caligares <mcaligares@gmail.com>
2017-11-27 13:03:05 -0300

    agregado metodo para crear cuenta de prueba en bootstrap

--- grails-app/init/org/munaylab/BootStrap.groovy
+++ grails-app/init/org/munaylab/BootStrap.groovy
@@ -1,12 +1,35 @@
 package org.munaylab
 
 import org.munaylab.user.User
+import org.munaylab.user.TipoUsuario
+import org.munaylab.osc.Organizacion
+import org.munaylab.osc.EstadoOrganizacion
+import org.munaylab.osc.TipoOrganizacion
+import org.munaylab.osc.UserOrganizacion
+
+import grails.util.Environment
 
 class BootStrap {
 
     def init = { servletContext ->
         log.info "initializing..."
 
+        environments {
+            development {
+                crearOrganizacionParaPruebas()
+            }
+        }
+
+    }
+
+    void crearOrganizacionParaPruebas() {
+        User user = new User(username: 'mcaligares@gmail.com', nombre: 'Augusto',
+            apellido: 'Caligares', password: 'Pass1234!')
+        Organizacion org = new Organizacion(nombre: 'MunayLab', tipo: TipoOrganizacion.FUNDACION, estado: EstadoOrganizacion.REGISTRADA,
+            objeto: 'Brindar herramientas innovadoras a las organizaciones de la sociedad civil.')
+        UserOrganizacion admin = new UserOrganizacion(user: user, organizacion: org, tipo: TipoUsuario.ADMINISTRADOR)
+        org.addToAdmins(admin)
+        org.save(failOnError: true)
     }
 
     def destroy = {
